cc4167ccecaabdbb3a2a89f9dd437d0013ff93e4
simoneamirato <160018316+simoneamirato@users.noreply.github.com>
2024-06-17 17:37:35 +0200

    Delete prova.py

--- prova.py
+++
@@ -1,6 +0,0 @@
-import AE_AAD
-import create_dataset_unsupervisied
-
-x_training,oracle,_ = create_dataset_unsupervisied.one_vs_all(0, 10)
-
-AE_AAD.launch(x_training,oracle,10,3,AE_type="shallow",EP=20,verb=1)
